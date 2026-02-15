8d2f3610560a4bbff835768b044fa5e2fd8aa351
okabe tomoyuki <nagi0110@yahoo.co.jp>
2026-02-15 22:07:17 +0900

    優待商品の複数選択と選択状態の引き継ぎを追加 (#17)

--- SharePerks/SharePerks.Client/Program.cs
+++ SharePerks/SharePerks.Client/Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Shareholder.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddScoped<RewardSelectionState>();
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
